2baee599cc811d2720227d967a043ca7ce0b429e
Hopeson <hopesonogaga@gmail.com>
2023-12-19 17:02:36 +0100

    header ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@
 module.exports = {
   content: ["./build/*.html", "./build/js/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        very_Dark_Blue: "hsl(235, 21%, 11%)",
+        Very_Dark_Desaturated_Blue: "hsl(235, 24%, 19%)",
+        Light_Grayish_Blue: "hsl(234, 39%, 85%)",
+        Light_Grayish_Blu_hover: "hsl(236, 33%, 92%)",
+        Dark_Grayish_Blue: "hsl(234, 11%, 52%)",
+        Very_Dark_Grayish_Blue: "hsl(233, 14%, 35%)",
+        Very_Dark_Grayish_Blue: "hsl(237, 14%, 26%)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
